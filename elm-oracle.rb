2c59c9e47e473bd72701d1b70f9510170356d9a3
Cj-bc <cj-bc@y-modify.org>
2019-05-28 09:27:03 +0900

    Fix: exec brew audit --strict

--- elm-oracle.rb
+++ elm-oracle.rb
@@ -4,7 +4,6 @@ class ElmOracle < Formula
   desc "Query for information about values in elm source files"
   homepage "https://github.com/ElmCast/elm-oracle#readme"
   url "https://registry.npmjs.org/elm-oracle/-/elm-oracle-1.1.1.tgz"
-  version "1.1.1"
   sha256 "627ea0ef226f02ffbb3079f0c7c7cef33dc837700faa0d2a8d61141094a323c6"
 
   depends_on "node"
